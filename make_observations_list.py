22ac71fabeb6b9f94d00e662260cd73077a35c09
Alex Viana <alexcostaviana@gmail.com>
2013-07-10 15:48:16 -0400

    Rough but working version for local test.

--- make_observations_list.py
+++ make_observations_list.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+import glob
 import os
 
 from database_interface import session
@@ -8,11 +9,22 @@ from database_interface import IR_FLT_0
 
 query = session.query(Master, IR_FLT_0).\
     join(IR_FLT_0, Master.id == IR_FLT_0.id).\
-    order_by(IR_FLT_0.EXPSTART).all()
-
+    order_by(IR_FLT_0.EXPSTART)
 
 test = True
 if test:
     
-for record in query[0:25]:
-    print os.path.join(record.Master.dir, record.Master.filename)
+    sorted_local_files = []
+    local_file_list = glob.glob('../data/*/Visit*/*.fits')
+    for local_file in local_file_list:
+        local_query = query.filter(Master.filename == os.path.basename(local_file)).one()
+        sorted_local_files.append((local_file, local_query.IR_FLT_0.EXPSTART)) 
+    #for record in query[0:25]:
+    #    print os.path.join(record.Master.dir, record.Master.filename)
+
+
+    sorted_local_files = sorted(sorted_local_files, key=lambda item : item[1])
+
+    with open('per_list.txt', 'w') as f:
+        for line in sorted_local_files:
+            f.write(line[0] + '\n')
